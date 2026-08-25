e497a85ca4cdb0988ba92bb1539ffbdc4af1ff86
MrRocky <101849785+MrRockySL@users.noreply.github.com>
2026-08-25 19:17:55 +0530

    Update the cask to Muro 3.0
    Point at the v3.0 DMG and its checksum, add a livecheck block so new
    releases are picked up, quit a running Muro before replacing the app,
    and zap the wallpaper extension container and URL session storage that
    2.0 and later leave behind.

--- Casks/muro.rb
+++ Casks/muro.rb
@@ -1,20 +1,29 @@
 cask "muro" do
-  version "2.0"
-  sha256 "ddc86db7a71b9bf22c2c89a4bfece62b5d10ca02f0ecc31faeffe1f1899a6899"
+  version "3.0"
+  sha256 "a7d5e901268c6604aa8ffba06045dd50d273f4f7290342d46b929eb32a6f094e"
 
   url "https://github.com/MrRockySL/Muro/releases/download/v#{version}/Muro-#{version}.dmg"
   name "Muro"
   desc "Native live video wallpapers with multi-display support"
   homepage "https://github.com/MrRockySL/Muro"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on arch: :arm64
   depends_on macos: :sonoma
 
   app "Muro.app"
 
+  uninstall quit: "com.mrrockysl.muro"
+
   zap trash: [
     "~/Library/Application Support/Muro",
     "~/Library/Caches/Muro",
+    "~/Library/Containers/com.mrrockysl.muro.wallpaper-extension",
+    "~/Library/HTTPStorages/com.mrrockysl.muro",
     "~/Library/Preferences/com.mrrockysl.muro.plist",
   ]
 end
